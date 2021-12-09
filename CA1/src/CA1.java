291ca29aec0f0688a46f34035927dc0f9d0bcffc
Farley Reis <farleyhard@hotmail.com>
2021-12-09 02:09:22 +0000

    mudan......

--- CA1/src/CA1.java
+++ CA1/src/CA1.java
@@ -7,9 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
-
-
+//todas as mudancas devem aparecer assim, olha como isso ja vai direto para o diretorio
 
 public class CA1 extends JPanel {
 
